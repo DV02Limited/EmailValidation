37b4d546a90a009d9bdd94b47cbcec8af9cba1d5
Simon O'Hara <SOHara@dv02.co.uk>
2017-08-02 14:21:18 +0100

    Add project files.

---
+++ EmailValidation/EmailValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailValidation
+{
+    public class EmailValidation
+    {
+        public int ValidateEmail (string emailHeader, string emailSender, string expectedSender)
+        {
+            var regexheader = new Regex(@"Usage");
+
+            if (regexheader.IsMatch(emailHeader) == true && emailSender == expectedSender)
+            {
+                return 0;
+            }
+            else
+                return 1;
+        }
+    }
+}
